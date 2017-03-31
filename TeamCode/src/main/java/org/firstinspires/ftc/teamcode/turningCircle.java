95f171241ff0d216bfdf3c44b55317acf84444fb
Ted Wang <tie.xiao.wang@gmail.com>
2017-03-31 16:07:10 -0700

    turning OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turningCircle.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+import ftc.backcode.OpModeBase;
+/**
+ * Created by admin on 3/30/2017.
+ */
+
+public class turningCircle extends OpModeBase{
+
+    //Radius from pole to the edge of the robot
+    //Measurement in Inches
+    int turningRadius = 39;
+    public void loop(){
+        if (gamepad1.dpad_up){
+            turningRadius++;
+        }
+        else if (gamepad1.dpad_down){
+            turningRadius--;
+        }
+        left1.setPower(turningRadius/(turningRadius-13));
+        left2.setPower(turningRadius/(turningRadius-13));
+        right1.setPower(-1);
+        right2.setPower(-1);
+        telemetry.addData("Radius:", turningRadius);
+    }
+}
